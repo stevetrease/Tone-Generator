2c762ed5ee1b70e51e535745a9d3eca6497cd783
Steve <stevetrease@gmail.com>
2019-03-04 11:44:21 +0000

    First sound!

--- Tone Generator/ViewController.swift
+++ Tone Generator/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
+import AudioKit
+import AudioKitUI
+
+
 class ViewController: UIViewController {
 
+    var oscillator1 = AKOscillator()
+    var oscillator2 = AKOscillator()
+    var mixer = AKMixer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mixer = AKMixer(oscillator1, oscillator2)
+        mixer.volume = 0.5
+        AudioKit.output = mixer
+        
+        AKSettings.playbackWhileMuted = true
+        
+        do {
+            try AudioKit.start()
+        } catch {
+            print("AudioKit did not start!")
+        }
+        
+        
+        oscillator1.amplitude = random(in: 0.5...1.0)
+        oscillator1.frequency = random(in: 220...880)
+        print (oscillator1.amplitude, oscillator1.frequency)
+        oscillator1.start()
+
     }
 
 
