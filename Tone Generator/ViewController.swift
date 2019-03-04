061b1d612caa798b3418dcc535ff2b7a254472aa
Steve <stevetrease@gmail.com>
2019-03-04 12:39:55 +0000

    Working sort of.

--- Tone Generator/ViewController.swift
+++ Tone Generator/ViewController.swift
@@ -12,19 +12,33 @@ import AudioKit
 import AudioKitUI
 
 
+
 class ViewController: UIViewController {
 
     var oscillator1 = AKOscillator()
-    var oscillator2 = AKOscillator()
     var mixer = AKMixer()
+    
+    
+    @IBOutlet weak var OSC1Switch: UISwitch!
+    @IBOutlet weak var OSC1Waveform: UISegmentedControl!
+    @IBOutlet weak var OSC1Frequency: UIStepper!
+    @IBOutlet weak var OSC1FrequencyLabel: UILabel!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
         
-        mixer = AKMixer(oscillator1, oscillator2)
+        mixer = AKMixer(oscillator1)
         mixer.volume = 0.5
         AudioKit.output = mixer
+        
+        oscillator1.amplitude = 0.5
+        oscillator1.rampDuration = 0.1
+        // oscillator1.frequency = OSC1Frequency.value
+        // oscillator1.start()
         
         AKSettings.playbackWhileMuted = true
         
@@ -34,14 +48,34 @@ class ViewController: UIViewController {
             print("AudioKit did not start!")
         }
         
-        
-        oscillator1.amplitude = random(in: 0.5...1.0)
-        oscillator1.frequency = random(in: 220...880)
-        print (oscillator1.amplitude, oscillator1.frequency)
-        oscillator1.start()
-
+        updateOSC1()
     }
-
+    
+    
+    @IBAction func OSC1changed (sender: AnyObject) {
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
+        updateOSC1()
+    }
+    
+    func updateOSC1() {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 0
+        
+        oscillator1.frequency = OSC1Frequency.value
+        OSC1FrequencyLabel.text = formatter.string(from: NSNumber(value: oscillator1.frequency))
+        
+        if OSC1Switch.isOn {
+            oscillator1.start()
+        } else {
+            oscillator1.stop()
+        }
+        
+        
+        
+        
+        print (oscillator1.amplitude, oscillator1.frequency, oscillator1.isStarted)
+    }
 
 }
 
